a09edc72baaf0c12f6d669e41f12b3a902338653
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-10-31 21:12:13 +0900

    Connect TabNavigator to all pages

---
+++ src/pages/Summary.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default class Summary extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Summary Page!!</Text>
+      </View>
+    )
+  }
+}
---
+++ src/pages/Timer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default class Timer extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Timer Page!!</Text>
+      </View>
+    )
+  }
+}
