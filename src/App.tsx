f9f36a0807b70e0e60bbefe2e0ec769d1dd2f9ae
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-11-01 00:19:39 +0900

    Add reducers to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import React from 'react'
 import { TabNavigator } from 'react-navigation'
+import { Provider } from 'react-redux'
 import Settings from './pages/Settings'
 import Summary from './pages/Summary'
 import Timer from './pages/Timer'
 import Todo from './pages/Todo'
+import store from './store'
 
-export default TabNavigator({
+const AppNavigator = TabNavigator({
   Todo: { screen: Todo },
   Timer: { screen: Timer },
   Summary: { screen: Summary },
   Settings: { screen: Settings }
 })
+
+export default () => (
+  <Provider store={store}>
+    <AppNavigator />
+  </Provider>
+)
