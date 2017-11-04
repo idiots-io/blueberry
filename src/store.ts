3e015237c8de63a8181d7314905a442d3fafcf25
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-11-04 23:43:32 +0900

    Add redux-logger and add it to middleware

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,6 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { persistStore, persistCombineReducers } from 'redux-persist'
+import logger from 'redux-logger'
 import storage from 'redux-persist/es/storage' // default: localStorage if web, AsyncStorage if react-native
 import reducers from './reducers' // where reducers is a object of reducers
 
@@ -8,10 +9,12 @@ const config = {
   storage
 }
 
+const createStoreWithMiddleware = applyMiddleware(logger)(createStore)
+
 const reducer = persistCombineReducers(config, { reducers })
 
 export default () => {
-  const store = createStore(reducer)
+  const store = createStoreWithMiddleware(reducer)
   const persistor = persistStore(store)
   return { persistor, store }
 }
