1284f8e5aeac7af959181e97836cde628ce3aa64
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-11-12 12:55:33 +0900

    Add style for SectionHeader

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -8,6 +8,7 @@ import Header from '../components/Header'
 import FilterAndSearch from '../components/FilterAndSearch'
 import AddBlueberryBtn from '../components/AddBlueberryBtn'
 import TodoListItem from '../components/TodoListItem'
+import TodoListSectionHeader from '../components/TodoListSectionHeader'
 import { SwipeListView } from 'react-native-swipe-list-view'
 
 namespace TodoComponent {
@@ -27,11 +28,62 @@ class TodoComponent extends React.Component<
   constructor(props) {
     super(props)
     const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
+      rowHasChanged: (r1, r2) => r1 !== r2,
+      sectionHeaderHasChanged: (s1, s2) => s1 !== s2
     })
     this.state = {
       ds,
-      dataSource: ds.cloneWithRows([...props.todos])
+      // dataSource: ds.cloneWithRowsAndSections([...props.todos])
+      dataSource: ds.cloneWithRowsAndSections({
+        '2017/10/08': [
+          {
+            text: 'Study React',
+            sessionCount: 38
+          },
+          {
+            text: 'Study Typescript',
+            sessionCount: 12
+          }
+        ],
+        '2017/11/04': [
+          {
+            text: 'Work on Resume',
+            sessionCount: 3
+          },
+          {
+            text: 'Git commit',
+            sessionCount: 42
+          }
+        ],
+        '2017/11/02': [
+          {
+            text: 'Work on Resume',
+            sessionCount: 3
+          },
+          {
+            text: 'Git commit',
+            sessionCount: 42
+          },
+          {
+            text: 'Work on Resume',
+            sessionCount: 3
+          },
+          {
+            text: 'Git commit',
+            sessionCount: 42
+          }
+        ],
+        '2017/11/01': [
+          {
+            text: 'Work on Resume',
+            sessionCount: 3
+          },
+          {
+            text: 'Git commit',
+            sessionCount: 42
+          }
+        ]
+      })
     }
   }
 
@@ -52,7 +104,13 @@ class TodoComponent extends React.Component<
         />
         <SwipeListView
           dataSource={this.state.dataSource}
-          renderRow={todo => <TodoListItem text={todo} />}
+          renderRow={todo => (
+            <TodoListItem text={todo.text} sessionCount={todo.sessionCount} />
+          )}
+          disableRightSwipe
+          renderSectionHeader={(_, category) => (
+            <TodoListSectionHeader date={category} />
+          )}
           renderHiddenRow={() => (
             <View
               style={{
