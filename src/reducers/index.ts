301c44f3a1abfad7378fe4031e239bdfa72dce9a
gloorx <glrx.vloq@gmail.com>
2017-11-14 23:30:37 +0900

    Add dummy data

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,18 @@
+import { times, sample } from 'lodash';
+
 const DEFAULT_STATE: State = {
-  sessions: [],
-  todos: [],
+  sessions: times(40, num => ({
+    id: num.toString(),
+    duration: num,
+    createdAt: new Date,
+    todoId: sample([0, 1, 2, 3]).toString(),
+  })),
+  todos: times(4, num => ({
+    id: num.toString(),
+    title: `Todo #${num}`,
+    isDone: (num % 2 === 0),
+    createdAt: new Date(),
+  })),
   settings: {
     workInterval: {
       labelKorean: '세션 길이',
@@ -18,26 +30,27 @@ const DEFAULT_STATE: State = {
 }
 
 interface Session {
-  id: String
-  duration: Number
+  id: string
+  duration: number
   createdAt: Date
-  todoId: Number
+  todoId: string,
 }
 
 export interface Todo {
-  title: String
-  isDone: Boolean
+  id: string;
+  title: string
+  isDone: boolean
   createdAt: Date
 }
 
 interface State {
   sessions: Session[]
   todos: Todo[]
-  settings: Object
+  settings: object
 }
 
 export interface Action {
-  type: String
+  type: string
   payload: any
 }
 
